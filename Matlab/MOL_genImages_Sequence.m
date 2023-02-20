abac3f9291a1004312479f7fce43944ef8cd63bb
matthijsoudelohuis <matthijsoudelohuis@gmail.com>
2023-02-20 14:30:33 +0000

    fixed bug with image presentation

--- Matlab/MOL_genImages_Sequence.m
+++ Matlab/MOL_genImages_Sequence.m
@@ -13,14 +13,8 @@ trials.trialnum = transpose(1:par.nSelec_images*par.repetitions);
 
 %% Generate 
 
-seq = randperm(par.nTotal_images,par.nSelec_images);
+seq = randperm(par.nTotal_images,par.nSelec_images)-1;
 trials.imagenum = [seq'; seq'];
-
-%% Create and Save table
-%Table
-table_trials = cell2table(sequence, 'VariableNames', variable_names);
-%Save
-writetable(table_trials,'TrialSequence_Goncalo_ultimate.csv')
 
 %% Create and save the table:
 table_trials = struct2table(trials);
