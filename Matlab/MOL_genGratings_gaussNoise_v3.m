ea2de5b05ad63238fb999fc8fe24aadb065b98ab
Matthijs Oude Lohuis <matthijsoudelohuis@gmail.com>
2023-11-16 15:53:00 +0000

    updated nosie protocol because of ordering issue csv

--- Matlab/MOL_genGratings_gaussNoise_v3.m
+++ Matlab/MOL_genGratings_gaussNoise_v3.m
@@ -7,7 +7,7 @@ par.Contrast        = 0.7; %0-1 contrast
 par.Phase           = 0; %0-1 starting phase
 
 nOris               = length(par.ori);
-nSpeeds             = length(par.Speeds);
+nSpeeds             = length(par.speed);
 
 %Noise dimensions (ori and speed)
 par.stdOri          = 20; %deg (span of degrees around center orientation)
@@ -30,7 +30,7 @@ par.ntrials         = nOris * nSpeeds * par.nrep;
 % 8,0,4,0.08,0.80,0
 % 9,210,2,0.04,0.80,0
 % 10,180,2,0.04,0.40,0
-% 11,330,2,0.04,0.80,0
+% 11,30,2,0.04,0.80,0
 % 12,210,2,0.04,0.80,0
 
 % PM: pref 1.5 Hz 0.08 cpd: 18.75 deg/s
@@ -53,6 +53,8 @@ b = fitlm(tbl);
 coef = table2array(b.Coefficients);
 plot(b)
 
+% coef = [-50;7.5];
+coef = [7.5;-50];
 targetspeed = 67;
 SF =  coef(1,1) / (targetspeed - coef(2,1));
 TF = coef(2,1)*SF + coef(1,1);
@@ -82,25 +84,31 @@ trials.Orientation = trials.Orientation + (rand(par.ntrials,1)-0.5)*par.stdOri;
 trials.Orientation = mod(trials.Orientation,360);
 
 %% Generate block-shuffled Speed:
-trials.Speed                   = []; %shuffle block (not stimulus side block)
+trialspeeds = [];
 for iRep = 1:par.nrep*nSpeeds
     order = randperm(nSpeeds); 
-    trials.Speed = [trials.Speed; par.speed(order)']; %Add trials to trialType vector in randomized order (block-scrambled)
+    trialspeeds = [trialspeeds; par.speed(order)']; %Add trials to trialType vector in randomized order (block-scrambled)
 end
 
 %add noise:
-temp = log10(trials.Speed);
+temp = log10(trialspeeds);
 temp = temp + (rand(par.ntrials,1)-0.5) * 2 * par.stdSpeed;
-trials.Speed = 10.^temp;
+trialspeeds = 10.^temp;
+
+
 
 %% Derive TF and SF from speed: 
-SF =  coef(1,1) ./ (trials.Speed - coef(2,1));
+SF =  coef(1,1) ./ (trialspeeds - coef(2,1));
 TF = coef(2,1)*SF + coef(1,1);
 figure(); set(gcf,'color','w')
 plot(SF,TF,'k.','MarkerSize',12)
 xlabel('Spatial Freq')
 ylabel('Temporal Freq')
 title('SF and TF with fixed speed')
+
+trials.TF                       = TF; % store TF
+trials.SF                       = SF; %SF
+trials.Speed                    = trialspeeds; % speed
 
 %% Rest:
 trials.Contrast     = repmat(par.Contrast,par.ntrials,1)    + (rand(par.ntrials,1)-0.5) * 2 * par.stdContr;
