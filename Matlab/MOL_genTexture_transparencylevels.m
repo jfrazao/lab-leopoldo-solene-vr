91039fb3408e09e043db18ae618891b85666d34f
Matthijs Oude Lohuis <matthijsoudelohuis@gmail.com>
2023-11-17 17:49:25 +0000

    update transparent stim and bonsai transparent levels

--- Matlab/MOL_genTexture_transparencylevels.m
+++ Matlab/MOL_genTexture_transparencylevels.m
@@ -1,7 +1,14 @@
 
 %% Parameters:
-Nsx             = 192;	% Size of background image is Nsy x Nsx
-Nsy             = 128;	
+% Nsx             = 192;	% Size of background image is Nsy x Nsx
+% Nsy             = 128;	
+
+pixpercm        = 10;
+stimlength      = 25;
+stimheight      = 8;
+Nsx             = stimlength * pixpercm;	% Size of fog image is Nsy x Nsx
+Nsy             = stimheight * pixpercm;	
+
 
 % Nsx             = 192;	% Size of synthetic image is Nsy x Nsx
 % Nsy             = 64;	% WARNING: Both dimensions must be multiple of 2^(Nsc+2)
@@ -18,6 +25,7 @@ rootDir         = 'C:\Users\Admin\Desktop\Bonsai\lab-leopoldo-solene-vr\workflow
 % imagefiles = {'seamless-circle-pattern-6543320.jpg','sd1000_5472.o.jpg', 'poly_grad.o.jpg', 'D20_c.o.jpg'};
 imagefiles = {'D3_c.o.jpg','sd1000_5472.o.jpg', 'poly_grad.o.jpg', 'D20_c.o.jpg'};
 
+alphamax = 128;
 Nimages = length(imagefiles);
 
 %% for images:
@@ -54,7 +62,7 @@ for iA = 1:length(alphas)
         imagedata = uint8(repmat(imdata(:,:,iM),1,1,3));
         alphadata = repmat(alphas(iA),Nsy,Nsx);
         
-        subplot(Nimages,length(contrasts),iA + (iM-1)*length(alphas))
+        subplot(Nimages,length(alphas),iA + (iM-1)*length(alphas))
         image(imagedata,'AlphaData',alphadata)
 
         title(alphas(iA),'fontsize',10)
@@ -70,7 +78,7 @@ for iA = 1:length(alphas)
     for iM=1:Nimages
 %         imagedata = uint8(imdata(:,:,iM));
         imagedata = uint8(repmat(imdata(:,:,iM),1,1,3));
-        alphadata = uint8(repmat(alphas(iA),Nsy,Nsx)*64);
+        alphadata = uint8(repmat(alphas(iA),Nsy,Nsx)*alphamax);
 %         alphadata(1,1) = 255;
         if ~exist(fullfile(rootDir,char(iM + 64)),'dir')
             mkdir(fullfile(rootDir,char(iM + 64)))
@@ -81,3 +89,91 @@ for iA = 1:length(alphas)
     end
 end
 
+%% 
+
+oris            = [45 135];
+
+Noris = length(oris);
+
+Par.dblScreenWidth_cm = 20;
+Par.dblScreenHeight_cm = 9;
+Par.dblScreenDistance_cm = 10;
+Par.intScreenHeight_pix = Nsy;
+Par.intScreenWidth_pix = Nsx;
+Par.dblSpatFreq = 0.08;
+
+dblScreenWidth_deg      = atand((Par.dblScreenWidth_cm / 2) / Par.dblScreenDistance_cm) * 2; 
+dblScreenHeight_deg     = atand((Par.dblScreenHeight_cm / 2) / Par.dblScreenDistance_cm) * 2;
+pixPerDeg               = Par.intScreenHeight_pix / dblScreenHeight_deg; %number of pixels in single retinal degree
+degreesPerCycle         = 1/Par.dblSpatFreq; %number of degrees in a single cycle (black-white block)
+pixPerCycle             = degreesPerCycle * pixPerDeg; %number of pixels in such a cycle
+
+
+%% create gratings:
+
+
+imdata = NaN(Nsy,Nsx,Noris);
+for iOri=1:Noris
+
+    dblRotAngle = oris(iOri);
+    dblPhase = 0;
+
+    imSize      = max([Par.intScreenWidth_pix,Par.intScreenHeight_pix]);
+    X           = 1:imSize;                     % X is a vector from 1 to imageSize
+    X0          = (X / imSize) - .5;            % rescale X -> -.5 to .5
+    freq        = imSize/pixPerCycle;           % compute frequency from wavelength
+    [Xm,Ym]     = meshgrid(X0, X0);             % 2D matrices
+    thetaRad    = (dblRotAngle / 360)*2*pi;     % convert theta (orientation) to radians
+    Xt          = Xm * cos(thetaRad);           % compute proportion of Xm for given orientation
+    Yt          = Ym * sin(thetaRad);           % compute proportion of Ym for given orientation
+    XYt         = Xt + Yt;                      % sum X and Y components
+    XYf         = XYt * freq * 2*pi;            % convert to radians and scale by frequency
+    thisFrameGrat = sin(XYf + dblPhase);        % make 2D sinewave
+
+    thisFrameGrat(thisFrameGrat>0) = 255;   % Convert into squared waves: white
+    thisFrameGrat(thisFrameGrat<0) = 0;   % Convert into squared waves: black
+    thisFrameGrat = thisFrameGrat(1:Par.intScreenHeight_pix,1:Par.intScreenWidth_pix); % Cut only screen portion of sq grat
+
+    imdata(:,:,iOri) = thisFrameGrat;
+end
+
+figure; set(gcf,'color','w','units','normalized','Position',[0.2,0.5,0.6,0.15])
+for iOri=1:Noris
+    subplot(1,Noris,iOri)
+    imshow(uint8(imdata(:,:,iOri)))
+end
+
+%%
+
+
+alphas = [0.01 0.01:0.01:1];
+for iA = 1:length(alphas)
+    for iOri=1:Noris
+%         imagedata = uint8(imdata(:,:,iM));
+        imagedata = uint8(repmat(imdata(:,:,iOri),1,1,3));
+        alphadata = uint8(repmat(alphas(iA),Nsy,Nsx)*alphamax);
+%         alphadata(1,1) = 255;
+        if ~exist(fullfile(rootDir,sprintf('Ori%d',oris(iOri))),'dir')
+            mkdir(fullfile(rootDir,sprintf('Ori%d',oris(iOri))))
+        end
+        filename = fullfile(rootDir,sprintf('Ori%d',oris(iOri)),sprintf('Ori%d_%03.0f.png',oris(iOri),iA-1));
+        imwrite(imagedata,filename,'png','Alpha',alphadata);
+%         imwrite(imagedata,filename,'png','Alpha',alphadata,'Background',0.5);
+    end
+end
+
+
+% contrasts = 0:0.01:1;
+% 
+% for iC = 1:length(contrasts)
+%     for iOri=1:Noris
+%         iMerge = uint8(contrasts(iC) * double(imdata(:,:,iOri)) + (1-contrasts(iC)) * double(BGnoise));
+% %         iMerge = uint8(iC * double(imdata(:,:,iM)) + (1-iC) * double(BGnoise));
+%         
+%         if ~exist(fullfile(rootDir,sprintf('Ori%d',oris(iOri))),'dir')
+%             mkdir(fullfile(rootDir,sprintf('Ori%d',oris(iOri))))
+%         end
+%         filename = fullfile(rootDir,sprintf('Ori%d',oris(iOri)),sprintf('Ori%d_%03.0f.jpg',oris(iOri),iC-1));
+%         imwrite(uint8(iMerge)',filename);
+%     end
+% end
