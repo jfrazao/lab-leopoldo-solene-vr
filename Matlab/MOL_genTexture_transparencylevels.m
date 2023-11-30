4c6ceafec0bcb2b2aee587512f99a6c81ccb891f
matthijsoudelohuis <matthijsoudelohuis@gmail.com>
2023-11-30 12:39:54 +0000

    mean matching of stimuli

--- Matlab/MOL_genTexture_transparencylevels.m
+++ Matlab/MOL_genTexture_transparencylevels.m
@@ -1,43 +1,43 @@
 
 %% Parameters:
-% Nsx             = 192;	% Size of background image is Nsy x Nsx
-% Nsy             = 128;	
-
 pixpercm        = 10;
-stimlength      = 25;
+stimlength      = 20;
 stimheight      = 8;
-Nsx             = stimlength * pixpercm;	% Size of fog image is Nsy x Nsx
+Nsx             = stimlength * pixpercm;	% Size of image is Nsy x Nsx
 Nsy             = stimheight * pixpercm;	
-
-
-% Nsx             = 192;	% Size of synthetic image is Nsy x Nsx
-% Nsy             = 64;	% WARNING: Both dimensions must be multiple of 2^(Nsc+2)
-
-% noise params:
-Nsmoothing      = 10;
-Nwindow         = 50;
 
 % Texture params:
 rootDir         = 'T:\Bonsai\lab-leopoldo-solene-vr\workflows\Textures\detection_stim\';
-rootDir         = 'C:\Users\Admin\Desktop\Bonsai\lab-leopoldo-solene-vr\workflows\Textures\detection_stim\';
+% rootDir         = 'C:\Users\Admin\Desktop\Bonsai\lab-leopoldo-solene-vr\workflows\Textures\detection_stim\';
 % imagefiles = {'seamless-circle-pattern-6543320.jpg','sawtooth-grating.o.jpg','d30_1923.o.jpg','30D_3500.o.jpg'};
 
 % imagefiles = {'seamless-circle-pattern-6543320.jpg','sd1000_5472.o.jpg', 'poly_grad.o.jpg', 'D20_c.o.jpg'};
 imagefiles = {'D3_c.o.jpg','sd1000_5472.o.jpg', 'poly_grad.o.jpg', 'D20_c.o.jpg'};
-
-alphamax = 128;
 Nimages = length(imagefiles);
 
-%% for images:
+%for the transparency, value that is fully opaque:
+alphamax = 128;
+
+%% For images:
 imdata = NaN(Nsy,Nsx,Nimages);
 for i=1:Nimages
     im = double(imread(fullfile(rootDir,imagefiles{i})));	
     if ndims(im)==3
         im = mean(im,3);
     end
-    im = im - min(im(:)); im = (im / max(im(:)))*255; 
-
     imdata(:,:,i) = im(1:Nsy,1:Nsx);
+end
+
+for i=1:Nimages
+    im = imdata(:,:,i);
+    
+    im = im - min(im(:)); im = (im / max(im(:)))*255; 
+        
+    im = im / (mean(im(:)) / 128);
+
+    im(im>255) = 255; 
+
+    imdata(:,:,i) = im;
 end
 
 figure; set(gcf,'color','w','units','normalized','Position',[0.2,0.5,0.6,0.15])
@@ -46,10 +46,48 @@ for i=1:Nimages
     imshow(uint8(imdata(:,:,i)))
 end
 
+%% Show spectrum as well for the images: 
+figure; set(gcf,'color','w','units','normalized','Position',[    0.2594    0.0898    0.4354    0.5907])
+for i=1:Nimages
+    subplot(3,Nimages,i)
+    imshow(uint8(imdata(:,:,i)))
+end
+for i=1:Nimages
+    subplot(3,Nimages,i+Nimages)
+    
+    N = 256;
+    im = imdata(:,:,i);
+    % It will be easiest for this exercise if the image is square, so if its not already square you should truncate the largest dimension.  Now, to compute the power spectrum of the image you use the Fourier transform, which converts a function in the space domain to a function in the frequency domain.  You can compute the Fourier transform of an image in Matlab using fft2 as follows:
+    imf=fftshift(fft2(im));
+    % The function fftshift is needed to put the DC component (frequency = 0) in the center.  The power spectrum is just the square of the modulus of the Fourier transform,  which is obtained as follows:
+    impf=abs(imf).^2;
+    % To display the power spectrum you will need to define some frequency coordinates.  If the image is of size N, then the frequencies run from -N/2 to N/2-1 (assuming N is even):
+    f=-N/2:N/2-1;
+    % Then display the log power spectrum using imagesc:
+    % figure()
+    imagesc(f,f,log10(impf)), axis xy
+    % You will note that the power falls off with frequency as you move away from the center.  Ignore the vertical and horizontal streak for now - its an artifact due to the boundaries of the image.  Now, to get a better idea of how the power falls off, we can do a rotational average of the power spectrum:
+    % Pf=rotavg(impf);
+    set(gca,'XTick',[],'YTick',[]);
+
+    [f1,Pf]=radial_profile(impf,2);
+
+    subplot(3,Nimages,i+Nimages*2)
+
+    % Now define a frequency coordinate from 0 to N/2:
+    % f1=0:N/2;
+    % and plot the rotationally averaged spectrum on a log-log plot:
+    % figure()
+%     loglog(f1,Pf)
+    semilogy(f1,Pf)
+end
+
 %% Show histogram of pixel intensities:
 figure; set(gcf,'color','w','units','normalized','Position',[0.2,0.5,0.4,0.3]); hold all;
 for iM=1:Nimages
     plot(0:254,histcounts(imdata(:,:,iM),0:255,'normalization','pdf'),'linewidth',2); 
+    im = imdata(:,:,iM);
+    plot([mean(im(:)) mean(im(:))],[0 0.25],'k','linewidth',2); 
 end
 title('Pixel intensities','fontsize',15)
 
@@ -89,7 +127,7 @@ for iA = 1:length(alphas)
     end
 end
 
-%% 
+%% Generate grating stimuli:
 
 oris            = [45 135];
 
