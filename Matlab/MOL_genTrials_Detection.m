fc0ae3abd5b8dfb2dc382a73e23232ad0e921c88
Matthijs Oude Lohuis <matthijsoudelohuis@gmail.com>
2023-12-14 17:37:34 +0000

    updated rewards for noise and added first trial 100% signal

--- Matlab/MOL_genTrials_Detection.m
+++ Matlab/MOL_genTrials_Detection.m
@@ -18,6 +18,7 @@ par.fracs           = [0.2 0.8];
 par.signals         = [0 100];
 
 trials.signal   = createDetectionTrialVector(par);
+trials.signal(1) = par.signals(end); %first trial max signal
 
 %% trials.reward
 trials.RewardTrial = trials.signal > 0;
@@ -34,6 +35,7 @@ nconds          = length(par.signals);
 par.fracs       = repmat(1/nconds,nconds,1);
 
 trials.signal   = createDetectionTrialVector(par);
+trials.signal(1) = par.signals(end); %first trial max signal
 
 %% trials.reward
 trials.RewardTrial = trials.signal > 0;
@@ -56,6 +58,8 @@ trials.signal(idx) = trials.signal(idx) + (rand(sum(idx),1)-0.5)*par.stdsignal;
 
 fprintf('\nJitter ranges from %2.0f to %2.0f %% signal\n',min(trials.signal(idx)),max(trials.signal(idx)))
 trials.signal       = round(trials.signal);
+
+trials.signal(1) = par.signals(end); %first trial max signal
 
 %% trials.reward
 trials.RewardTrial = trials.signal > 0;
